75dadb933c66825f99e149d9babb883a4423789f
huangjing <289860219@qq.com>
2019-04-29 14:47:20 +0800

    rest

---
+++ src/main/java/com/mycompany/myapp/repository/DepartmentRepository.java
@@ -0,0 +1,16 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.Department;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Department entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    void findByDepartmentName(String s);
+}
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'region',
+                loadChildren: './region/region.module#BlogRegionModule'
+            },
+            {
+                path: 'country',
+                loadChildren: './country/country.module#BlogCountryModule'
+            },
+            {
+                path: 'location',
+                loadChildren: './location/location.module#BlogLocationModule'
+            },
+            {
+                path: 'department',
+                loadChildren: './department/department.module#BlogDepartmentModule'
+            },
+            {
+                path: 'task',
+                loadChildren: './task/task.module#BlogTaskModule'
+            },
+            {
+                path: 'employee',
+                loadChildren: './employee/employee.module#BlogEmployeeModule'
+            },
+            {
+                path: 'job',
+                loadChildren: './job/job.module#BlogJobModule'
+            },
+            {
+                path: 'job-history',
+                loadChildren: './job-history/job-history.module#BlogJobHistoryModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
